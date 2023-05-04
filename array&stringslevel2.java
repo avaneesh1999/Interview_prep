c94f5e457186a348d46cdc806171e2b8e6dea0e9
Avaneesh <avaneeshsingh1999@gmail.com>
2023-05-04 12:17:43 +0530

    leetcode26

--- array&stringslevel2.java
+++ array&stringslevel2.java
@@ -48,4 +48,21 @@ public class Solution {
 
 
      }
+
+     //leetcode 26. Remove Duplicates from Sorted Array
+     //https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+
+     public int removeDuplicates(int[] nums) {
+        int i=0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+
+    
 }

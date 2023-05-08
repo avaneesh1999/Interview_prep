661f12edbc3c6d140b64e753d3274b82b7a1595d
Avaneesh <avaneeshsingh1999@gmail.com>
2023-05-08 16:55:29 +0530

    leetcode 628

--- array&stringslevel2.java
+++ array&stringslevel2.java
@@ -292,6 +292,45 @@ public class Solution {
         
     }
 
+    //628. Maximum Product of Three Numbers
+    //https://leetcode.com/problems/maximum-product-of-three-numbers/description/
+    public int maximumProduct(int[] nums) {
+        //intialize three max and two min
+        int max1=Integer.MIN_VALUE;
+        int max2=Integer.MIN_VALUE;
+        int max3=Integer.MIN_VALUE;
+
+        int min1=Integer.MAX_VALUE;
+        int min2=Integer.MAX_VALUE;
+
+        for(int i=0;i<nums.length;i++){
+            // check for max
+            if(max1<nums[i]){
+                max3=max2;
+                max2=max1;
+                max1=nums[i];
+            }else if(max2<nums[i]){
+                max3=max2;
+                max2=nums[i];
+            }else if(max3<nums[i]){
+                max3=nums[i];
+            }
+
+            //check for min
+            if(min1>nums[i]){
+                min2=min1;
+                min1=nums[i];
+            }else if(min2 >nums[i]){
+                min2=nums[i];
+            }
+        }
+
+        return Math.max(max1*max2*max3,min1*min2*max1);
+
+
+        
+    }
+
 
     
 }
